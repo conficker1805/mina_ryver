eff635bee1e6ea7c594b50c17dd29d88e77c9ad4
quang <phanhaiquang@gmail.com>
2017-05-20 19:19:55 +0700

    change text message & clean up old codes regarding to old mina version

--- lib/mina_slack/tasks.rb
+++ lib/mina_slack/tasks.rb
@@ -5,19 +5,47 @@ require 'json'
 
 # ### slack_api_token
 # Sets the slack api auth token.
-set :slack_api_token, ''
+def _slack_api_token
+  fetch(:slack_api_token, '')
+end
 
 # ### slack_channels
 # Sets the channels where notifications will be sent to.
-set :slack_channels, []
+def _slack_channels
+  fetch(:slack_channels, [])
+end
 
 # ### slack_username
 # Sets the notification 'from' user label
-set :slack_username, 'Deploy'
+def _slack_username
+  fetch(:slack_username, 'Deploy username')
+end
 
-# ### slack_author
-# Sets the deployment author name
-set :slack_author, 'Someone'
+def _slack_attachments
+  fetch(:slack_attachments, '[{"fields": [{"value": "Value"}]}]')
+end
+
+def _link_names
+  fetch(:link_names)
+end
+
+def _branch
+  fetch(:branch, "")
+end
+
+def _slack_application
+  fetch(:slack_application, "")
+end
+
+def _text_at_start
+  fetch(:text_at_start, "Start deploying branch #{_branch} to #{_slack_application}")
+end
+
+def _text_at_finished
+  text = fetch(:text_at_finished, "Finished deploying branch #{_branch} to #{_slack_application}")
+  text += " See it here: #{_slack_domain}" if _slack_domain != nil
+  text
+end
 
 # ### slack_link_names
 # Sets the deployment author name
@@ -25,15 +53,25 @@ set :slack_link_names, 1
 
 # ### slack_parse
 # Sets the deployment author name
-set :slack_parse, 'full'
+def _slack_parse
+  fetch(:slack_parse, 'full')
+end
 
 # ### icon_url
 # URL to an image to use as the icon for this message
-set :slack_icon_url, ''
+def _slack_icon_url
+  fetch(:slack_icon_url, '')
+end
 
 # ### icon_emoji
 # Sets emoji to use as the icon for this message. Overrides `slack_icon_url`
-set :slack_icon_emoji, ':slack:'
+def _slack_icon_emoji
+  fetch(:slack_icon_emoji, ':slack:')
+end
+
+def _slack_domain
+  fetch(:slack_domain, nil)
+end
 
 # ## Control Tasks
 namespace :slack do
@@ -41,13 +79,12 @@ namespace :slack do
   # ## slack:notify_deploy_started
   desc "Send slack notification about new deploy start"
   task :notify_deploy_started => :environment do
-    queue  %[echo "-----> Sending start notification to Slack"]
-    text = "#{slack_author} is deploying #{application}..."
+    command  %[echo "-----> Sending start notification to Slack"]
 
-    for channel in slack_channels
+    for channel in _slack_channels
       send_message(
         channel: channel,
-        text:    text
+        text:    _text_at_start
       )
     end
   end
@@ -55,22 +92,19 @@ namespace :slack do
   # ## slack:notify_deploy_finished
   desc "Send slack notification about deploy finish"
   task :notify_deploy_finished => :environment do
-    queue  %[echo "-----> Sending finish notification to Slack"]
+    command  %[echo "-----> Sending finish notification to Slack"]
 
-    text  = "#{slack_author} finished deploying #{application}."
-    text += " See it here: http://#{domain}" if domain != nil
-
-    for channel in slack_channels
+    for channel in _slack_channels
       send_message(
         channel:     channel,
-        text:        text,
-        attachments: attachments
+        text:        _text_at_finished,
+        attachments: _slack_attachments
       )
     end
   end
 
   def send_message(params = {})
-    queue %[curl -X POST https://slack.com/api/chat.postMessage -d "token=#{slack_api_token}&channel=#{params[:channel]}&username=#{slack_username}&text=#{params[:text]}&attachments='[{"fields": [{"value": "Value"}]}]'&parse=#{slack_parse}&link_names=#{link_names}&icon_url=#{slack_icon_url}&icon_emoji=#{slack_icon_emoji}" --silent > /dev/null]
+    command %[curl -X POST https://slack.com/api/chat.postMessage -d "token=#{_slack_api_token}&channel=#{params[:channel]}&username=#{_slack_username}&text=#{params[:text]}&attachments=params[:attachments]&parse=#{_slack_parse}&link_names=#{_link_names}&icon_url=#{_slack_icon_url}&icon_emoji=#{_slack_icon_emoji}" --silent > /dev/null]
   end
 
 end
